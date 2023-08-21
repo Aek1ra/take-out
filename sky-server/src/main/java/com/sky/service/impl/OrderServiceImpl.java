a50b1c50fffee943eb9223ad7afc79bc59cccfb5
Stringzhua <2785649457@qq.com>
2023-08-22 01:26:53 +0800

    客户下单催单消息提醒

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -23,6 +23,7 @@ import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
 
+import com.sky.websocket.WebSocketServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,6 +54,8 @@ public class OrderServiceImpl implements OrderService {
     private AddressBookMapper addressBookMapper;
     @Autowired
     private WeChatPayUtil weChatPayUtil;
+    @Autowired
+    private WebSocketServer webSocketServer;
 
     /**
      * 用户下单
@@ -174,6 +177,16 @@ public class OrderServiceImpl implements OrderService {
                 .build();
 
         orderMapper.update(orders);
+        //通过WebSocket向客户端浏览器推送消息type orderId content
+        Map map=new HashMap();
+        map.put("type",1);
+        //1表示来单提醒
+        //2表示客户催单
+        map.put("orderId",ordersDB.getId());
+        map.put("content","订单号"+outTradeNo);
+
+        String json = JSON.toJSONString(map);
+        webSocketServer.sendToAllClient(json);
     }
     /**
      * 用户端订单分页查询
@@ -494,6 +507,27 @@ public class OrderServiceImpl implements OrderService {
         orders.setDeliveryTime(LocalDateTime.now());
 
         orderMapper.update(orders);
+    }
+
+    /**
+     * 客户催单
+     * @param id
+     */
+    public void reminder(Long id) {
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+
+        // 校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+        Map map=new HashMap();
+        map.put("type",2);
+        //1来单提醒,2客户接单
+        map.put("orderId",id);
+        map.put("content","订单号:"+ordersDB.getNumber());
+        //通过websocke向客户端浏览器来推送消息
+        webSocketServer.sendToAllClient(JSON.toJSONString(map));
     }
     @Value("${sky.shop.address}")
     private String shopAddress;
--- sky-server/src/main/java/com/sky/task/MyTask.java
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -1,10 +1,8 @@
 package com.sky.task;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.juli.logging.Log;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import java.util.Date;
 
 /**
@@ -13,11 +11,13 @@ import java.util.Date;
 @Component
 @Slf4j
 public class MyTask {
+
     /**
-     * 定时任务,每隔5s触发一次
+     * 定时任务 每隔5秒触发一次
      */
-    @Scheduled(cron = "0/5 * * * * ?")
+    //@Scheduled(cron = "0/5 * * * * ?")
     public void executeTask(){
-        log.info("定时任务开始执行:{}",new Date());
+        log.info("定时任务开始执行：{}", new Date());
     }
+
 }
